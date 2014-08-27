072f8f17f4aae77a129e250f6d80375c558e42cf
PearlsMedia <PearlsMedia@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-08-27 12:20:36 +0000

    update
    git-svn-id: https://plugins.svn.wordpress.org/xfile/trunk@973928 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- xcom_Head_Renderer.php
+++ xcom_Head_Renderer.php
@@ -157,3 +157,13 @@ if($javascriptPlugins && count($javascriptPlugins)){
 }else{
 
 }
+?>
+<!--Start of Zopim Live Chat Script-->
+<script type="text/javascript">
+	window.$zopim||(function(d,s){var z=$zopim=function(c){z._.push(c)},$=z.s=
+		d.createElement(s),e=d.getElementsByTagName(s)[0];z.set=function(o){z.set.
+	_.push(o)};z._=[];z.set._=[];$.async=!0;$.setAttribute('charset','utf-8');
+		$.src='//v2.zopim.com/?2NTMTET7LTUMmledW9IT55fQkq6DeptG';z.t=+new Date;$.
+			type='text/javascript';e.parentNode.insertBefore($,e)})(document,'script');
+</script>
+<!--End of Zopim Live Chat Script-->
